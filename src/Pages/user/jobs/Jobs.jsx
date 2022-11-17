4d5c2caeda642a532aeccb2561f2bab8589134bf
muhmdajeer <muhdajeer@gmail.com>
2022-11-17 17:28:45 +0530

    post menu added

--- src/Pages/user/jobs/Jobs.jsx
+++ src/Pages/user/jobs/Jobs.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useEffect, useState } from "react"
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 
 import Navbar from "../../../components/NavBar/Navbar"
@@ -7,7 +7,7 @@ import JobCard from "../../../components/user/jobCard/JobCard"
 import Loader from '../../../components/Loader'
 import { getJobs } from "../../../redux/actions/jobs"
 import './Jobs.scss'
-import SkeletonElement from "../../../skeletons/SkeletonElement"
+
 import JobCardSkeleton from "../../../skeletons/JobCardSkeleton"
 import JobDetilsSkeleton from "../../../skeletons/JobDetilsSkeleton"
 
@@ -17,68 +17,64 @@ import JobDetilsSkeleton from "../../../skeletons/JobDetilsSkeleton"
 const Jobs = () => {
 
   const dispatch = useDispatch()
-  const [loading,setLoading] = useState(true)
-  const [jobs,setJobs] = useState([])
-  const [selectedJob,setSelectedJob] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [selectedJob, setSelectedJob] = useState({})
 
 
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      dispatch(getJobs(setLoading,setJobs))
-
-    },35000)
+  useEffect(() => {
+    dispatch(getJobs(setLoading, setJobs))
     setSelectedJob(jobs[0])
-    console.log(jobs[0]);
-  },[])
+  }, [])
 
 
-  if(loading){
-    
-    return(
+  if (loading) {
+
+    return (
       <>
-      <Navbar/>
-      <div className="jobs_container">
-        <div className="jobs_skeleton">
-  <JobCardSkeleton/>
-  <JobCardSkeleton/>
-  <JobCardSkeleton/>
+        <Navbar />
+        <div className="jobs_container">
+          <div className="jobs_skeleton">
+            <JobCardSkeleton />
+            <JobCardSkeleton />
+            <JobCardSkeleton />
+          </div>
+          <div className="job_details_skeleton">
+            <JobDetilsSkeleton />
+          </div>
         </div>
-        <div className="job_details_skeleton">
-  <JobDetilsSkeleton/>
-        </div>
-      </div>
       </>
     )
   }
 
   return (
     <Fragment>
-    <Navbar/>
-    <div className="jobs_container">
+      <Navbar />
+      <div className="jobs_container">
         <div className="jobs">
-            {
-              jobs.map((job)=> (
-                <JobCard setJob={setSelectedJob} job={job} />
-              ))
-            }
+          {
+            jobs.map((job) => (
+              <JobCard setJob={setSelectedJob} job={job} />
+            ))
+          }
         </div>
-        <div  className="job_details">
+        <div className="job_details">
           <div className="jd_container">
             <div className="jd_top">
 
-            <div className="jd_top_left">
-              <h1>{selectedJob?.jobRole}</h1>
-              <h3>Google.Inc</h3>
-              <h4>{selectedJob?.location}</h4>
-            </div>
-            <div className="jd_top_right">
+              <div className="jd_top_left">
+                <h1>{selectedJob?.jobRole}</h1>
+                <h3>Google.Inc</h3>
+                <h4>{selectedJob?.location}</h4>
+              </div>
+              <div className="jd_top_right">
 
-              <img src="../google.png" alt="" />
-            </div>
+                <img src="../google.png" alt="" />
+              </div>
             </div>
 
-          <hr />
+            <hr />
             <div className="jd_mid">
               <h4>Job details</h4>
 
@@ -101,7 +97,7 @@ const Jobs = () => {
               <h4>Required Skills</h4>
               <div className="skills">
                 {
-                  selectedJob?.skills?.map((skill)=>(
+                  selectedJob?.skills?.map((skill) => (
                     <div className="skill">{skill}</div>
                   ))
                 }
@@ -113,8 +109,8 @@ const Jobs = () => {
             </div>
           </div>
         </div>
-    </div>
-   </Fragment>
+      </div>
+    </Fragment>
   )
 }
 
--- src/components/home/Post/Post.jsx
+++ src/components/home/Post/Post.jsx
@@ -1,47 +1,84 @@
 import moment from "moment";
 
 import "./post.scss";
-import { MoreVert, ThumbUp, Comment, SendRounded, MoreHoriz, ThumbUpOffAltOutlined} from "@mui/icons-material";
-import { useEffect, useState } from "react";
+import { MoreVert, ThumbUp, Comment, SendRounded, MoreHoriz, ThumbUpOffAltOutlined } from "@mui/icons-material";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllPosts, likePost , addComment} from "../../../redux/actions/posts";
+import { getAllPosts, likePost, addComment } from "../../../redux/actions/posts";
+import { useRef } from "react";
+import Modal from 'react-modal'
+import { ListItemButton, Menu, MenuItem } from "@mui/material";
 
 export default function Post({ post }) {
   console.log(post);
-  const [commentOpen,setCommentOpen] = useState(false)
-  const [comment,setComment] = useState('')
-  const [liked,setLiked] = useState(false)
+  const [commentOpen, setCommentOpen] = useState(false)
+  const [comment, setComment] = useState('')
+  const [liked, setLiked] = useState(false)
   const dispatch = useDispatch()
-  const updated = useSelector((state)=>state.posts.updated)
+  const updated = useSelector((state) => state.posts.updated)
+  const [ownPost, setOwnPost] = useState(false)
 
-  const handleLike = (postId)=>{
-    dispatch(likePost(postId,setLiked))
+
+  const handleLike = (postId) => {
+    dispatch(likePost(postId, setLiked))
   }
 
-  const handleComment = ()=>{
-    dispatch(addComment(comment,post._id))
+  const handleComment = () => {
+    dispatch(addComment(comment, post._id))
     setComment('')
   }
-   
-  useEffect(()=>{
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user'))
+
+    if (post.user._id === user?.user?._id) {
+      console.log('//////////////////////////');
+      setOwnPost(true)
+      console.log({ ownPost });
+    }
     setLiked(post?.likes?.includes(user?.user?._id))
-  },[post])
+  }, [post])
 
   return (
     <div className="post">
+
       <div className="post_wrapper">
         <div className="post_top">
           <div className="post_top_left">
-            <img className="post_profile_img" src={`${post.user?.profileImg ? post.user?.profileImg : '../avatarIcon.jpg' }`} alt="" />
+            <img className="post_profile_img" src={`${post.user?.profileImg ? post.user?.profileImg : '../avatarIcon.jpg'}`} alt="" />
             <div>
 
-            <span className="post_username">{post?.user?.lastName}</span>
-            <span className="post_date">{moment(post.createdAt).fromNow()}</span>
+              <span className="post_username">{post?.user?.lastName}</span>
+              <span className="post_date">{moment(post.createdAt).fromNow()}</span>
             </div>
           </div>
           <div className="post_top_right">
-            <MoreVert className="more_icon" />
+            {ownPost && 
+            <MoreVert onClick={handleClick} className="more_icon" />
+            }
+            <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Edit</MenuItem>
+        <MenuItem onClick={handleClose}>Delete</MenuItem>
+      </Menu>
+
           </div>
         </div>
         <div className="post_center">
@@ -51,13 +88,13 @@ export default function Post({ post }) {
         <div className="post_bottom">
           <div className="post_bottom_left">
             {
-              liked ? 
-              <ThumbUp onClick={()=>handleLike(post._id)} className="like_btn" />
-              :
-              <ThumbUpOffAltOutlined onClick={()=>handleLike(post._id)} className="like_btn" />
+              liked ?
+                <ThumbUp onClick={() => handleLike(post._id)} className="like_btn" />
+                :
+                <ThumbUpOffAltOutlined onClick={() => handleLike(post._id)} className="like_btn" />
             }
-          
-            <Comment onClick={()=>setCommentOpen(!commentOpen)} className="like_btn" />
+
+            <Comment onClick={() => setCommentOpen(!commentOpen)} className="like_btn" />
             <span className="post_like_counter">{post?.likes?.length} people like it</span>
           </div>
           <div className="post_bottom_right">
@@ -65,46 +102,46 @@ export default function Post({ post }) {
           </div>
         </div>
         {/* comment section */}
-      {
-        commentOpen &&
-      <div className="comment_container">
-        <div className="comment_input">
-          <img  src="../avatar.jpeg" alt="" />
-          <textarea  autoFocus value={comment} onChange={(event)=>setComment(event.target.value)}  placeholder="Enter a comment" type="text" />
-          {
-          comment.length > 0 &&
-          <SendRounded  onClick={handleComment} className="sendIcon" />
-          }
-        </div>
-        <hr />
-        <div className="comments_box">
-          {
-            post.comments[0] && post.comments
-            .map((comment)=>(
-              <CommentCard comment={comment} />
-            ))
-          }
-        </div>
-      </div>
-      }
+        {
+          commentOpen &&
+          <div className="comment_container">
+            <div className="comment_input">
+              <img src="../avatar.jpeg" alt="" />
+              <textarea autoFocus value={comment} onChange={(event) => setComment(event.target.value)} placeholder="Enter a comment" type="text" />
+              {
+                comment.length > 0 &&
+                <SendRounded onClick={handleComment} className="sendIcon" />
+              }
+            </div>
+            <hr />
+            <div className="comments_box">
+              {
+                post.comments[0] && post.comments
+                  .map((comment) => (
+                    <CommentCard comment={comment} />
+                  ))
+              }
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
 }
 
-const CommentCard = ({comment})=>{
-  return(
+const CommentCard = ({ comment }) => {
+  return (
     <div className="comment_card" >
-      <img src={`${comment.user?.profileImg ? comment.user?.profileImg : '../avatarIcon.jpg' }`} alt="" />
+      <img src={`${comment.user?.profileImg ? comment.user?.profileImg : '../avatarIcon.jpg'}`} alt="" />
       <div className="comment_details">
         <div className="comm_details_left">
-        <span className="commenter">{comment?.user.lastName}</span>
-        <span>{comment?.comment}</span>
+          <span className="commenter">{comment?.user.lastName}</span>
+          <span>{comment?.comment}</span>
         </div>
         <div className="comm_details_right">
           {console.log(comment.commentedAt)}
-        <span>{moment(comment?.commentedAt).fromNow()}</span>
-        <MoreHoriz/>
+          <span>{moment(comment?.commentedAt).fromNow()}</span>
+          <MoreHoriz />
         </div>
       </div>
     </div>
