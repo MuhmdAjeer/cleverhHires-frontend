8bcd252bb6dc331c6a99a7a28ce2219c6f953e1d
muhmdajeer <muhdajeer@gmail.com>
2022-11-26 22:27:12 +0530

    profile page created and job page ui problem resolved

--- src/Pages/user/profile/Profile.jsx
+++ src/Pages/user/profile/Profile.jsx
@@ -1,22 +1,49 @@
-import Navbar from '../../../components/NavBar/Navbar'
-import './Profile.scss'
 
 
-const Profile = () => {
+import "./Profile.scss";
+// import Topbar from "../../components/topbar/Topbar";
+// import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../../components/home/Feed";
+import {RightBar} from '../../../components/home/rightbar/RightBar'
+// import Rightbar from "../../components/rightbar/Rightbar";
+import Navbar from "../../../components/NavBar/Navbar";
+
+export default function Profile() {
   return (
     <>
-    <Navbar/>
-    <div className="profile_container">
-        <div className="wallpaper"></div>
-        <div className="profile_image">
-            <img src="avatar.jpeg" alt="" />
-        </div>
-        <div className="profile_meta">
-        <span>Muhammed Ajeer</span>
-        </div>
-    </div>
-    </>
-  )
-}
+      <Navbar />
+      <div className="home_container profile_home ">
 
-export default Profile
+      <div className="profile">
+        
+        
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="https://wallpaperaccess.com/full/2068754.jpg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="./avatar.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Muhammed Ajeer</h4>
+              <span className="profileInfoDesc">Full Stack developer</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            {/* <Feed /> */}
+            
+          </div>
+        </div>
+      </div>
+      <RightBar/>
+      </div>
+    </>
+  );
+}
