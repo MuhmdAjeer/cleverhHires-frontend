358a2b219cecde343aa7d3fcdda6b6e7018c4c13
muhmdajeer <muhdajeer@gmail.com>
2022-11-29 12:55:45 +0530

    about modaladded

--- src/Pages/user/profile/Profile.jsx
+++ src/Pages/user/profile/Profile.jsx
@@ -1,15 +1,30 @@
-
+import { useState } from "react";
 
 import "./Profile.scss";
-// import Topbar from "../../components/topbar/Topbar";
-// import Sidebar from "../../components/sidebar/Sidebar";
-import Feed from "../../../components/home/Feed";
 import { RightBar } from '../../../components/home/rightbar/RightBar'
-// import Rightbar from "../../components/rightbar/Rightbar";
 import Navbar from "../../../components/NavBar/Navbar";
-import { Edit } from "@mui/icons-material";
+import { Cancel, Edit } from "@mui/icons-material";
+import Modal from '../../../components/modal/Modal'
+
+const MODAL_STYLE = {
+  position: "fixed",
+  display: "flex",
+  width: "40%",
+  top: "50%",
+  borderRadius: "10px",
+  left: "50%",
+  transform: "translate(-50%,-50%)",
+  backgroundColor: "#fff",
+  padding: "20px",
+  zIndex: 1000,
+  height : 'auto'
+};
 
 export default function Profile() {
+
+  const [openEditAbout,setOpenAbout] = useState(false)
+  const [openEditExperience,setOpenExperience] = useState(false)
+
   return (
     <>
       <Navbar />
@@ -36,6 +51,7 @@ export default function Profile() {
               <div className="profileInfo">
                 <h4 className="profileInfoName">Muhammed Ajeer</h4>
                 <span className="profileInfoDesc">Full Stack developer</span>
+                <span className="follow_meta" >5 Followers | 9 Following</span>
               </div>
             </div>
             <div className="profileRightBottom">
@@ -43,23 +59,81 @@ export default function Profile() {
 
             </div>
           </div>
-          <div className="profile_about">
-            <div className="pro_about_top">
-            <h3>About</h3>
-            <Edit htmlColor="grey" />
+          <div className="profile_meta_card">
+            <div className="pro_meta_top">
+              <h3>About</h3>
+              <Edit onClick={()=>setOpenAbout(true)} htmlColor="grey" />
+              <Modal containerStyle={MODAL_STYLE} open={openEditAbout}  >
+                <div className="create_post_modal">
+                  <div className="modal_top">
+                    <span className="modal_header">Edit About</span>
+                    <Cancel onClick={()=>setOpenAbout(false)} className="cancel_icon" />
+                  </div>
+                  <div className="modal_body">
+                    <div className="edit_about_container">
+                      <span>You can write about your years of experience, industry, or skills. People also talk about their achievements or previous job experiences.</span>
+                      <div>
+                      <textarea required className='job_description' name="description" id="" cols="63" rows="15"></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="modal_bottom">
+                    <div className="modal_actions">
+                      <button className="btn_done">
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </Modal>
             </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsa illo. Vitae harum esse eligendi dolor quae, aut inventore quod nemo saepe molestiae accusantium exercitationem quas rem aliquam quos similique natus id quia perferendis aliquid voluptatibus totam ipsa fuga aperiam?</p>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsa illo. Vitae harum esse eligendi dolor quae, aut inventore quod nemo saepe molestiae accusantium exercitationem quas rem aliquam quos similique natus id quia perferendis aliquid voluptatibus totam ipsa fuga aperiam?</span>
           </div>
-          <div className="profile_education">
-            <div className="pro_edu_top">
-            <h3>About</h3>
-            <Edit htmlColor="grey" />
+          <div className="profile_meta_card">
+            <div className="pro_meta_top">
+              <h3>Experience</h3>
+              <Edit  htmlColor="grey" />
+              
+
             </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsa illo. Vitae harum esse eligendi dolor quae, aut inventore quod nemo saepe molestiae accusantium exercitationem quas rem aliquam quos similique natus id quia perferendis aliquid voluptatibus totam ipsa fuga aperiam?</p>
+            <ExperienceCard />
+            <ExperienceCard />
+            <ExperienceCard />
+            <ExperienceCard />
+
           </div>
+
+          <div className="profile_meta_card">
+            <div className="pro_meta_top">
+              <h3>Skills</h3>
+              <Edit htmlColor="grey" />
+            </div>
+            <div className="skills">
+
+              <div className="skill">Nodejs</div>
+              <div className="skill">MongoDB</div>
+              <div className="skill">React</div>
+            </div>
+
+          </div>
+
         </div>
         <RightBar />
       </div>
     </>
   );
+}
+
+function ExperienceCard() {
+  return (
+    <div className="exp-card" >
+      <img src="./google.png" alt="" />
+      <div className="exp-meta">
+        <h4>Backend Developer</h4>
+        <h5>Google</h5>
+        <span>Jun 2022 - Present · 6 mos</span>
+        <span>Banglore,India</span>
+      </div>
+    </div>
+  )
 }
