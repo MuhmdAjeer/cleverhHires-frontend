0c9f1f49d9c0579eab4bf7f2207486811dd94066
muhmdajeer <muhdajeer@gmail.com>
2022-12-25 15:39:37 +0530

    refactored

--- src/Pages/chat/Chat.jsx
+++ src/Pages/chat/Chat.jsx
@@ -22,7 +22,7 @@ export const Chat = () => {
 
 
   useEffect(()=>{
-    socket.current = io('https://emarald.online/socket')
+    socket.current = io('https://emarald.online/socket').connect()
     socket.current.emit('new-user-add',user._id)
     socket.current.on('get-users',(users)=>{
       setOnlineUsers(users)
