2e5c55f9ac68367d8dfc66471dc896bdbffc0da6
muhmdajeer <muhdajeer@gmail.com>
2022-12-25 14:57:36 +0530

    refactored

--- src/api/chats.js
+++ src/api/chats.js
@@ -4,7 +4,7 @@ const user = JSON.parse(localStorage.getItem("user"));
 
 const token = user?.token;
 export const API = axios.create({
-  baseURL: "https://localhost:5000/api/v1/chat",
+  baseURL: "https://emarald.online/api/v1/chat",
 });
 
 export const userChats = () => API.get(`/${user.user._id}`);
